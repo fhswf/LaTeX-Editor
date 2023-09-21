673939f608fee7e4293055861fc26722aca06306
github action <noreply>
2023-09-21 12:27:30 +0000

    update

--- docs/Konferenzseminar/config.js
+++ docs/Konferenzseminar/config.js
@@ -1,4 +1,4 @@
 var config_template_name = "Konferenzseminar";
 var config_main_tex_file = "Seminararbeit.tex";
 var config_project_files = ["IEEEtran.cls","Seminararbeit.tex","deeplearning.png","literatur.bib",];
-var config_placeholders = ["Titel der __ Seminararbeit","Max Mustermann",];
+var config_placeholders = ["Titel der Seminararbeit","Ihr Name","Hier sollte Ihre Kurzzusammenfassung stehen ... ",];
